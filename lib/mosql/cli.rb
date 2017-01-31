79b3751ce2c1acfc57c7bf4a9f91cb9564fd30f4
Justin Bean <justin.bean@usefulsystemsinc.com>
2017-01-30 22:19:23 -0700

    ssl compatibility

--- lib/mosql/cli.rb
+++ lib/mosql/cli.rb
@@ -121,7 +121,7 @@ module MoSQL
     end
 
     def connect_mongo
-      @mongo = Mongo::MongoClient.from_uri(options[:mongo])
+      @mongo = Mongo::MongoClient.from_uri(options[:mongo], ssl_opts(options[:mongo]))
       config = @mongo['admin'].command(:ismaster => 1)
       if !config['setName'] && !options[:skip_tail]
         log.warn("`#{options[:mongo]}' is not a replset.")
@@ -175,6 +175,12 @@ module MoSQL
       unless options[:skip_tail]
         @streamer.optail
       end
+    end
+
+    private
+  
+    def ssl_opts(uri)
+      options[:mongo].match /ssl=true/ ? {ssl: true, ssl_verify: false} : {}
     end
   end
 end
