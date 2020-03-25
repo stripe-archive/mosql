56bedfad56acf858ba3096c82eed33a00c34849e
Ketan Rathod <ketan@haystack.tv>
2020-03-24 17:09:10 -0700

    removing the creds from git

--- haystack/deploy.sh
+++ haystack/deploy.sh
@@ -1,40 +1,89 @@
 #!/bin/bash
+
+help () {
+	echo "Use $0 -pu <postgres-user> -pp <postgres-pwd> -ph <postgres-host> 
+			-mu <mongo-user> -mp <mongo-pwd> -mh <mongo-host> <dir-name>"
+}
 if [ $# -lt 1 ]; then
     echo "Your command line contains $# arguments"
-    echo "Use $0 [dir_name]"
+    help
     exit 0
 fi
-service_name=$1
-set -x
-echo 'hello world'
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-# TEST CREDENTIALS
-pg_user='mosqluser'
-pg_pwd='hzMzH2wnax9gTeY5'
-pg_url='test-dashboard.cgqtgkpz8uca.us-east-1.rds.amazonaws.com'
-mongo_user='oploguser'
-mongo_pwd='53r5rIn74ocjrwNs'
-mongo_url='SG-haystackdbec2-1718.servers.mongodirector.com'
-yml_dir="mosql-gem\/haystack\/$service_name"
+case $key in
+    -pu|--pg-user)
+    PGUSER="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -pp|--pg-pwd)
+    PGPWD="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -ph|--pg-host)
+    PGHOST="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -mu|--mo-user)
+    MOUSER="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -mp|--mo-pwd)
+    MOPWD="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -mh|--mo-host)
+    MOHOST="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)    # help
+    help
+    exit 0
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)    # unknown option
+	POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+# set -x
+set -- "${POSITIONAL[@]}" # restore positional parameters
+if [[ -z "$PGUSER" || -z "$PGPWD" || -z "$PGHOST" || -z "$MOUSER" || -z "$MOPWD" || -z "$MOHOST" || -z "$1" ]]
+then
+    help
+else
+	SERVICE_NAME="$1"
+	YML_DIR="mosql-gem\/haystack\/$SERVICE_NAME"
+    echo "PGUSER       = ${PGUSER}"
+	echo "PGPWD        = ${PGPWD}"
+	echo "PGHOST       = ${PGHOST}"
+	echo "MOUSER       = ${MOUSER}"
+	echo "MOPWD        = ${MOPWD}"
+	echo "MOHOST       = ${MOHOST}"
+	echo "SERVICE NAME = ${SERVICE_NAME}"
+	echo "YML_DIR.     = ${YML_DIR}"
+	cat etc/init/mosql-init.conf.template | sed 's/##pg_user##/'"$PGUSER"'/g; s/##pg_pwd##/'"$PGPWD"'/g; ' \
+	| sed 's/##pg_url##/'"$PGHOST"'/g; s/##mongo_user##/'"$MOUSER"'/g; ' \
+	| sed 's/##mongo_pwd##/'"$MOPWD"'/g; s/##mongo_url##/'"$MOHOST"'/g; ' \
+	| sed 's/##yml_dir##/'"$YML_DIR"'/g; s/##service_name##/'"$1"'/g; ' \
+	> etc/init/mosql-init.conf
 
-# PROD CREDENTIALS
-# pg_user='mosqluser'
-# pg_pwd='hzMzH2wnax9gTeY5'
-# pg_url='dashboarddbinstance.cgqtgkpz8uca.us-east-1.rds.amazonaws.com'
-# mongo_user='oploguser'
-# mongo_pwd='53r5rIn74ocjrwNs'
-# mongo_url='SG-haystackdbec2-1718.servers.mongodirector.com'
-# yml_dir="mosql-gem\/haystack\/$service_name"
+	sudo mkdir -p /etc/init
+	sudo cp etc/init/mosql-init.conf /etc/init
 
-cat etc/init/mosql-init.conf.template | sed 's/##pg_user##/'"$pg_user"'/g; s/##pg_pwd##/'"$pg_pwd"'/g; ' \
-| sed 's/##pg_url##/'"$pg_url"'/g; s/##mongo_user##/'"$mongo_user"'/g; ' \
-| sed 's/##mongo_pwd##/'"$mongo_pwd"'/g; s/##mongo_url##/'"$mongo_url"'/g; ' \
-| sed 's/##yml_dir##/'"$yml_dir"'/g; s/##service_name##/'"$service_name"'/g; ' \
-> etc/init/mosql-init.conf
-
-sudo mkdir -p /etc/init
-sudo cp etc/init/mosql-init.conf /etc/init
-
-# sudo initctl start mosql-init
-sudo initctl status mosql-init
-
+	# sudo initctl start mosql-init
+	sudo initctl status mosql-init
+fi
